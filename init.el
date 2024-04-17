86ca66ca12855c629bf2120f2c51bb324b3153bb
averagewagon <john.r.hendric@gmail.com>
2024-04-16 21:14:59 -0700

    Add packages to investigate

--- init.el
+++ init.el
@@ -6,7 +6,8 @@
       '( which-key
 	 corfu
 	 vertico
-	 marginalia))
+	 marginalia
+	 orderless))
 
 (setq package-native-compile t)
 
@@ -22,5 +23,28 @@
 (marginalia-mode)
 
 (require 'corfu)
-(setq corfu-auto t)
+(setq corfu-auto t
+      completion-styles '(orderless)
+      completion-category-defaults nil)
 (global-corfu-mode)
+
+;; TODO: Investigation additional packages
+;; magit
+;; dired-sidebar
+;; centaur-tabs
+;; gcmh
+;; rainbow-delimiters
+;; jinx
+;; vundo
+;; envrc
+;; editorconfig
+;; cape
+;; kind-icon
+;; consult
+;; yasnippet
+;; rg
+;; inheritenv
+;; adaptive-wrap
+;; devdocs
+;; eglot markdown-mode clang-format cmake-mode cargo
+;; toml-mode git-modes pdf-tools
